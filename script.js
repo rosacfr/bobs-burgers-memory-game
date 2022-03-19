3cf71611565ff0886ed2443c42ab27b3c984b834
rosacf-ga <rc.floress@yahoo.com>
2022-03-19 15:20:25 -0700

    Turn cards into an array

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-console.log('Hi') 
-
-let h3 = document.querySelector('h3');
-
-function sayCongrats() {
-    h3.innerText = 'Congratulations! You won!'
-}
-
-h3.addEventListener('click', sayCongrats)
+const cards = Array.from(document.querySelectorAll('.card'));
+console.log(cards);
