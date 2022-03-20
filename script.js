8e68ac789c304f8e41355c19a32f76ae268b97ad
rosacf-ga <rc.floress@yahoo.com>
2022-03-20 15:47:00 -0700

    Create function that flips cards back upon pressing start over button

--- script.js
+++ script.js
@@ -2,30 +2,24 @@ const startOverBtn = document.querySelector('#start-over');
 const h2El = document.querySelector('h2');
 const h3El = document.querySelector('h3');
 const gameContainer = document.querySelector('#game-container');
-//do i select the class of the photo, or the actual img? i.e. imgs/bob.jpg
-const faceUpCard = document.querySelectorAll('.front-card') //should these be an array?
+const faceUpCard = document.querySelectorAll('.front-card')
 const faceDownCard = document.querySelectorAll('.back-card')
 const cards = Array.from(document.querySelectorAll('.card')); // turns all cards into array
-//console.log(cards)
 let countdownEl = document.querySelector('#countdown')
 const startingMinutes = 2;
 let time = startingMinutes * 60; // 60 cos want all the seconds ( = 120)
 let cardPicked;
 let firstPick; 
 let secondPick;
-let intervalId //initializes timer //will put into startGame function //interal id is 1
-//arefaceUpCard and faceDown connected to cards array? so if iterating through is cards[i] okay? or will i have to make faceUpCards an array so it can read those image cards;
+let intervalId; //setInterval always has an id
 
 //event listeners
 
 startOverBtn.addEventListener('click', init)
 
 //_______________________________________________________________________________
- //initialize game without any event listeners
 
 //functions
-
-
 function initTimer (){
     const minutes = Math.floor(time /60); // 120/60 = 2
     let seconds = time % 60; //all seconds remaining after division of 120/60
@@ -34,20 +28,18 @@ function initTimer (){
     } else {
         countdownEl.innerHTML = `${minutes}:${seconds}`
     }
-    if(time > 0){
+    if(time > 0){ //will stop time once hits 0:00
     time --; 
     } else {
         clearInterval(intervalId);
     }
 }
 
-
 function flipCard(e) {
-    console.log(e.target);
     if (e.target.className === 'back-card') { //everything disappears
         e.target.style.opacity = '0';
         //only trigger timer when back card is clicked
-        if (!intervalId) { //if not defined will start timer
+        if (!intervalId) { //if not defined will start timer, i.e. only first card will start timer
             intervalId = setInterval(initTimer, 1000);
         }
     } else {
@@ -61,13 +53,10 @@ cards.forEach(function(card){
 
 function init(e) {
     shuffleCards();
-    //reset timer and interval function
-    time = 120;
-    countdownEl.innerText = '2:00'
-    clearInterval(intervalId);
-    intervalId = undefined;
+    resetTimer();
+    resetCards();
 }
-
+init();
 
 function shuffleCards () {
     for (let i=0; i<cards.length; i++){
@@ -76,14 +65,23 @@ function shuffleCards () {
     }
 }
 
-//look at time variable
+function resetTimer (){ 
+    time = 120;
+    countdownEl.innerText = '2:00'
+    clearInterval(intervalId);
+    intervalId = undefined;
+}
+
+function resetCards(){
+    faceDownCard.forEach(function(card){ //forEach cos an array
+        card.style.opacity = '1';
+    });
+}
+
 
 // function stopTimer (){
 // clearTimeout(intervalId);
 // } 
-
-//function resetTimer (){ 
-//}
 
 //function startGame(){
 // setInteral(initTimer,1000)
