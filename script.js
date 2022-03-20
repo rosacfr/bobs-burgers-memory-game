dbeff1c87e6ca17721b09483de8df3c4f89929e1
rosacf-ga <rc.floress@yahoo.com>
2022-03-20 14:10:31 -0700

    Fix issue with flipping card and optimise timer to start only when user clicks on initial card

--- script.js
+++ script.js
@@ -7,25 +7,66 @@ const faceUpCard = document.querySelectorAll('.front-card') //should these be an
 const faceDownCard = document.querySelectorAll('.back-card')
 const cards = Array.from(document.querySelectorAll('.card')); // turns all cards into array
 //console.log(cards)
-let countdown = document.querySelector('#countdown')
+let countdownEl = document.querySelector('#countdown')
 const startingMinutes = 2;
 let time = startingMinutes * 60; // 60 cos want all the seconds ( = 120)
+let cardPicked;
+let firstPick; 
+let secondPick;
+let intervalId //initializes timer //will put into startGame function //interal id is 1
 //arefaceUpCard and faceDown connected to cards array? so if iterating through is cards[i] okay? or will i have to make faceUpCards an array so it can read those image cards;
 
 //event listeners
-// cards.addeventListener('click', startTimer) <--- this might start timer EVERY time a card is clicked. How would I fix this?
 
 startOverBtn.addEventListener('click', init)
 
 //_______________________________________________________________________________
-init(); //initialize game without any event listeners
+ //initialize game without any event listeners
 
-// //functions
+//functions
+
+
+function initTimer (){
+    const minutes = Math.floor(time /60); // 120/60 = 2
+    let seconds = time % 60; //all seconds remaining after division of 120/60
+    if (seconds < 10){
+        countdownEl.innerHTML = `${minutes}:0${seconds}` //adds 0 to the end or else would be 2:0 instead of 2:00
+    } else {
+        countdownEl.innerHTML = `${minutes}:${seconds}`
+    }
+    if(time > 0){
+    time --; 
+    } else {
+        clearInterval(intervalId);
+    }
+}
+
+
+function flipCard(e) {
+    console.log(e.target);
+    if (e.target.className === 'back-card') { //everything disappears
+        e.target.style.opacity = '0';
+        //only trigger timer when back card is clicked
+        if (!intervalId) { //if not defined will start timer
+            intervalId = setInterval(initTimer, 1000);
+        }
+    } else {
+        return
+    }
+};
+
+cards.forEach(function(card){
+    addEventListener('click', flipCard);
+});
 
 function init(e) {
     shuffleCards();
+    //reset timer and interval function
+    time = 120;
+    countdownEl.innerText = '2:00'
+    clearInterval(intervalId);
+    intervalId = undefined;
 }
-// //timer set to 2 min
 
 
 function shuffleCards () {
@@ -35,27 +76,26 @@ function shuffleCards () {
     }
 }
 
-// setInterval(initTimer, 1000) //google what this does (it starts timer) //initializes timer
-
-function initTimer (){
-    const minutes = Math.floor(time /60); // 120/60 = 2
-    let seconds = time % 60; //all seconds remaining after division of 120/60
-    if (seconds < 10){
-        countdown.innerHTML = `${minutes}:0${seconds}` //adds 0 to the end or else would be 2:0 instead of 2:00
-    } else {
-        countdown.innerHTML = `${minutes}:${seconds}`
-    }
-    time --; //cos going down
-}
+//look at time variable
 
 // function stopTimer (){
+// clearTimeout(intervalId);
+// } 
 
+//function resetTimer (){ 
+//}
+
+//function startGame(){
+// setInteral(initTimer,1000)
+// checkMatch()
+// more functions etc etc ????????????????
 // }
 
 // function startTimer() 
 // //starts timer on click of first card
 
-// function flipCard() 
+
+
 // //flips card over 
 // //only 2 can be flipped at a time
 
